6b6972d2e5e6135cd03f0a3eb7859b6494385895
heruonan <m18013440282@163.com>
2025-10-16 22:07:39 +0800

    适配AutoDL镜像

--- tool/environment.sh
+++ tool/environment.sh
@@ -22,16 +22,16 @@
 
 # export CUDA_VISIBLE_DEVICES=2
 
-export TensorRT_Lib=/usr/lib/aarch64-linux-gnu
-export TensorRT_Inc=/usr/lib/aarch64-linux-gnu
-export TensorRT_Bin=/usr/src/tensorrt/bin
+export TensorRT_Lib=/root/TensorRT-8.6.1.6/targets/x86_64-linux-gnu/lib
+export TensorRT_Inc=/root/TensorRT-8.6.1.6/targets/x86_64-linux-gnu/include
+export TensorRT_Bin=/root/TensorRT-8.6.1.6/targets/x86_64-linux-gnu/bin
 
-export CUDA_Lib=/usr/local/cuda-11.4/lib64
-export CUDA_Inc=/usr/local/cuda-11.4/include
-export CUDA_Bin=/usr/local/cuda-11.4/bin
-export CUDA_HOME=/usr/local/cuda-11.4
+export CUDA_Lib=/usr/local/cuda-11.8/lib64
+export CUDA_Inc=/usr/local/cuda-11.8/include
+export CUDA_Bin=/usr/local/cuda-11.8/bin
+export CUDA_HOME=/usr/local/cuda-11.8
 
-export CUDNN_Lib=/usr/lib/aarch64-linux-gnu
+export CUDNN_Lib=/usr/lib/x86_64-linux-gnu
 
 
 # resnet50/resnet50int8/swint
